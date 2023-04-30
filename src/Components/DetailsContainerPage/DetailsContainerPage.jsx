5278fa9d29e4c5176a89fabcfc38151422801f1d
MartinMinuto <martin.minuto1@gmail.com>
2023-04-30 17:27:11 -0300

    2.3

--- src/Components/DetailsContainerPage/DetailsContainerPage.jsx
+++ src/Components/DetailsContainerPage/DetailsContainerPage.jsx
@@ -1,7 +1,23 @@
+import { useState, useEffect } from 'react'
+import { getProductsById } from '../../AsyncMock'
+import { useParams } from 'react-router-dom'
+import DetailsPage from '../DetailsPage/DetailsPage'
+
 const DetailsContainerPage = () => {
+    const [product, setProduct] = useState()
+
+    const { productId } = useParams()
+
+    useEffect(() => {
+       getProductsById(productId)
+         .then (product => {
+            setProduct(product)
+         })
+    }, [productId])
+
     return (
         <div>
-
+           <DetailsPage {...product}/>
         </div>
     )
 }
---
+++ src/Components/DetailsPage/DetailsPage.jsx
@@ -0,0 +1,24 @@
+import 'DetailsPage.css'
+
+const DetailsPage = ({name, price, img, category}) => {
+    return (
+        <div>
+            <div className='detailsContainer'>
+                <img className='detailsImg' src={`${img}`} alt={name}/>
+                <div className='detailsSubContainer'>
+                  <div className='detailsPositionContainer'>
+                    <h1 className='detailsName'>{name}</h1>
+                  </div>
+                  <div className='detailsPositionContainer'> 
+                    <h2 className='detailsCategory'>{category}</h2>
+                  </div>  
+                  <div className='detailsPositionContainer'>  
+                    <h3 className='detailsPrice'>$ {price}</h3>
+                  </div>   
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default DetailsPage
